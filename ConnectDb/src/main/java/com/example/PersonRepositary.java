dbd292896a8c619f0ac8230eb3fb56390921668a
gurushant.jidagi <gurushant.jidagi@afourtech.com>
2016-11-22 17:43:03 +0530

    hibernate spring boot application

---
+++ ConnectDb/src/main/java/com/example/PersonRepositary.java
@@ -0,0 +1,8 @@
+package com.example;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepositary extends JpaRepository<Person, Integer>
+{
+
+}
---
+++ ConnectDb/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+delete from person;
+insert into person(id,name,email) values
+(1,'Guru','Guru@gmail.com'),
+(2,'Jeevan','Jeevan@gmail.com')
+;
