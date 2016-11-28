6387f4a3cb86289a6709673ac073eaa6d25abf85
gurushant.jidagi <gurushant.jidagi@afourtech.com>
2016-11-28 18:27:06 +0530

    changed folder structure

---
+++ feedback_rating/src/test/java/com/feedback_rating/FeedbackRatingApplicationTests.java
@@ -0,0 +1,18 @@
+package com.feedback_rating;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FeedbackRatingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
+
